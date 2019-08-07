9d476ea47e115927c581d539a048daa68ed473d3
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-08-07 17:17:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@ const input = document.querySelector('input');
 input.addEventListener('keydown', function(e) {
   console.log(e.key);
 });
+
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+});
